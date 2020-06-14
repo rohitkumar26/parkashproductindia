b815310d7928810df74809e050105f4754a31d21
rohit <rkumar26384@outlook.com>
2020-06-14 16:22:53 +0530

    parkash product india

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.get('/', (req, res) => {
     res.redirect('invoices/invoicepage');
 })
 
-
+const host = '0.0.0.0';
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
